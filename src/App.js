39ac0eeceff9c77fc34372ee0cf00882da2df31c
micheaol <micheaol80@gmail.com>
2021-11-19 01:48:34 +0100

    add props to function components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import React from 'react';
+
 import './App.css';
 import { Form } from './components/Form';
 import { Nav } from './components/Nav';
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
-  }
-
-  render (){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Nav register="Register New"/>
-          <Form />
-        </header>
-      </div>
-    );
-  }
+ const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Nav register="Register New"/>
+        <Form />
+      </header>
+    </div>
+  );
+  
 }
 
 export default App;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,9 +2,7 @@
 import React from "react";
 
 export class Nav extends React.Component {
-    constructor(props){
-        super(props);
-    }
+
     render(){
         return(
             <div>
