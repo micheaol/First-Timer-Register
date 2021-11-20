83ed3121e49e2a21339a5e079b811238a5659950
micheaol <micheaol80@gmail.com>
2021-11-20 01:57:55 +0100

    feat: add state and props to the project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-
+import { useState } from 'react';
 import './App.css';
-import  Form  from './components/Form';
+import FirstTimerList from './components/FirstTimerList';
 import { Nav } from './components/Nav';
 
  const App = () => {
+
+  const [contacts, setContacts] = useState([
+    {
+      firstName: "Michael",
+      lastName: "Oladele",
+      phone: '012255421',
+      id: 1
+    },
+    {
+      firstName: "Rachel",
+      lastName: "Oladele",
+      phone: '012255421',
+      id: 2
+    },
+    {
+      firstName: "Matthew",
+      lastName: "Oladele",
+      phone: '012255421',
+      id: 3
+    },
+    {
+      firstName: "Light",
+      lastName: "Oladele",
+      phone: '012255421',
+      id: 4
+    },
+    {
+      firstName: "Joy",
+      lastName: "Emordi",
+      phone: '012255421',
+      id: 5
+    },
+    {
+      firstName: "Paul",
+      lastName: "Oladele",
+      phone: '012255421',
+      id: 6
+    },
+  ])
+
   return (
     <div className="App">
       <header className="App-header">
         <Nav register="Register New"/>
-        <Form firstName ="Your Name"/>
+        <FirstTimerList contacts={contacts}/>
+        {/* <Form firstName ="Your Name"/> */}
       </header>
     </div>
   );
---
+++ src/components/FirstTimerList.jsx
@@ -0,0 +1,32 @@
+import './FirstTimerList.css';
+
+const FirstTimerList = ({contacts}) => {
+
+    return ( 
+        <div className="first-timer-table">
+            <table class="table table-dark">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Firstname</th>
+                    <th scope="col">Lastname</th>
+                    <th scope="col">Phone</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        {contacts.map((contact)=>(
+                            <tr>
+                            <th scope="row">{contact.id}</th>
+                            <td>{contact.firstName}</td>
+                            <td>{contact.lastName}</td>
+                            <td>{contact.phone}</td>
+                            <button type="button" class="btn btn-dark">Dark</button>
+                        </tr>
+                        ))}
+                </tbody>
+            </table>
+        </div>
+     );
+}
+ 
+export default FirstTimerList;
