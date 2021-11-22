ae12e16b1c4ac829e15abd0aaae59de6cbb0fa0c
micheaol <micheaol80@gmail.com>
2021-11-22 16:12:58 +0100

    fix: fix navbar error

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,10 +9,8 @@ export class Nav extends React.Component {
         return(
             <div>
               <nav className="navbar navbar-expand-lg navbar-dark bg-dark my-nav">
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+               
+                <div id="navbarTogglerDemo01">
                     <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li className="nav-item">
                         <Link className="nav-link" to="/add-contact">{this.props.register} <span className="sr-only">(current)</span></Link>
@@ -29,3 +27,4 @@ export class Nav extends React.Component {
         )
     }
 }
+
